d66d1bc8399f1c72ea32514762c9a119abeddc88
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-01 22:14:52 +0600

    no-content section added

--- scripts/categories.js
+++ scripts/categories.js
@@ -3,18 +3,23 @@ const loadContents = async(id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await response.json();
     const contents = data.data;
+
+    const noContent = document.getElementById('no-content');
+    (contents.length === 0) ? noContent.classList.remove('hidden') : noContent.classList.add('hidden');
+
     contents.forEach( item => {
         cardLoader(item);
     })
+
     const buttons = document.getElementById('categories-container').children;
     for(const button of buttons){
-        if(button.id == id){
-            button.classList = `bg-[#FF1F3D] text-white text-sm md:text-base font-semibold py-1 px-5 rounded disabled:cursor-pointer`;
-            button.setAttribute('disabled','true');
-        }
-        else{
+        if(Number(button.id) !== Number(id)){
             button.classList = `bg-[#25252526] text-[#252525b3] text-sm md:text-base font-medium py-1 px-5 rounded disabled:cursor-pointer`;
             button.removeAttribute('disabled');
+        }
+        else{
+            button.classList = `bg-[#FF1F3D] text-white text-sm md:text-base font-semibold py-1 px-5 rounded disabled:cursor-pointer`;
+            button.setAttribute('disabled','true');
         }
     }
 }
@@ -38,4 +43,4 @@ const displayCategories = async() => {
 }
 
 displayCategories();
-loadContents(1000);
+loadContents('1000');
