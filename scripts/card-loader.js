fefd326783772f94a71a0d5705fb8a438bb4025c
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-01 20:38:03 +0600

    categories functionalities added

---
+++ scripts/card-loader.js
@@ -0,0 +1,74 @@
+const cardContainer = document.getElementById('card-container');
+const cardLoader = content =>{
+    // card-div
+    const outerDiv = document.createElement('div');
+    outerDiv.classList = `card rounded-none`;
+
+    // figure
+    const figure = document.createElement('figure');
+    figure.classList = `h-48 bg-[#000000ef] rounded-lg relative`;
+
+    // figure -> img
+    const cardImage = document.createElement('img');
+    cardImage.classList = `w-full`;
+    cardImage.setAttribute('src',`${content?.thumbnail}`);
+    figure.appendChild(cardImage);
+
+    // figure -> time-stamp
+    if(content?.others?.posted_date){
+        const seconds = Number(content.others.posted_date);
+        const hours = parseInt(seconds/3600);
+        const minutes = parseInt((seconds%3600)/60);
+        const timeStamp = document.createElement('p');
+        timeStamp.classList = `bg-[#171717] text-white absolute px-2 py-1 bottom-1 right-1 text-xs rounded`;
+        timeStamp.innerText = `${hours}hrs ${minutes}min ago`;
+        figure.appendChild(timeStamp);
+    }
+
+    outerDiv.appendChild(figure);
+
+    // card title div
+    const titleDiv = document.createElement('div');
+    titleDiv.classList = `flex items-center gap-2 mt-4`;
+
+    // card title div -> author's image
+    const authorImage = document.createElement('img');
+    authorImage.classList = `rounded-full w-10 h-10`;
+    authorImage.setAttribute('src',`${content?.authors?.[0]?.profile_picture}`);
+    titleDiv.appendChild(authorImage);
+
+    // card title div -> title
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList = `text-base font-bold`;
+    cardTitle.innerText = content.title;
+    
+    titleDiv.appendChild(cardTitle);
+    outerDiv.appendChild(titleDiv);
+
+    // author div
+    const authorDiv = document.createElement('div');
+    authorDiv.classList = `flex items-center gap-1 ml-12 my-2 text-[#171717b3] text-sm`;
+
+    // author div -> author name
+    const authorName = document.createElement('h3');
+    authorName.innerText = content?.authors?.[0]?.profile_name;
+    authorDiv.appendChild(authorName);
+
+    // author div -> verified
+    if(content?.authors?.[0]?.verified){
+        const verification = document.createElement('img');
+        verification.setAttribute('src','assets/images/verified.svg');
+        authorDiv.appendChild(verification);
+    }
+
+    outerDiv.appendChild(authorDiv);
+
+    // view count
+    const viewCount = document.createElement('h3');
+    viewCount.classList = `ml-12 text-[#171717b3] text-sm`;
+    viewCount.innerText = `${content?.others?.views} views`;
+    
+    outerDiv.appendChild(viewCount);
+
+    cardContainer.appendChild(outerDiv);
+}
--- scripts/categories-display.js
+++ scripts/categories-display.js
@@ -1,3 +1,24 @@
+const loadContents = async(id) => {
+    cardContainer.innerHTML = '';
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await response.json();
+    const contents = data.data;
+    contents.forEach( item => {
+        cardLoader(item);
+    })
+    const buttons = document.getElementById('categories-container').children;
+    for(const button of buttons){
+        if(button.id == id){
+            button.classList = `bg-[#FF1F3D] text-white text-sm md:text-base font-semibold py-1 px-5 rounded disabled:cursor-pointer`;
+            button.setAttribute('disabled','true');
+        }
+        else{
+            button.classList = `bg-[#25252526] text-[#252525b3] text-sm md:text-base font-medium py-1 px-5 rounded disabled:cursor-pointer`;
+            button.removeAttribute('disabled');
+        }
+    }
+}
+
 const displayCategories = async() => {
 
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
@@ -7,11 +28,14 @@ const displayCategories = async() => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(item => {
         const button = document.createElement('button');
-        button.classList = `bg-[#25252526] text-[#252525b3] text-sm md:text-base font-medium py-1 px-5 rounded`;
+        button.classList = `bg-[#25252526] text-[#252525b3] text-sm md:text-base font-medium py-1 px-5 rounded disabled:cursor-pointer`;
         button.innerText = item.category;
+        button.setAttribute('onclick',`loadContents(${item.category_id})`);
+        button.setAttribute('id',`${item.category_id}`);
         categoriesContainer.appendChild(button);
     });
-    
+  
 }
 
 displayCategories();
+loadContents(1000);
